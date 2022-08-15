3b6eb03c93032b9616c68284d7234faa5bb06ad6
Aviad Churaman <aviadchuraman@gmail.com>
2022-08-15 16:56:40 -0500

    Update main.js

--- main.js
+++ main.js
@@ -5,15 +5,21 @@ const volumeEl = document.getElementById("volume-el")
 const massEl = document.getElementById("mass-el")
 
 const meterToFeet =  3.281
+const feetToMeter = 0.3048
 const literToGallon =  0.264
+const gallonToLiter = 3.78541
 const kiloToPound =  2.204
+const poundToKilo = 0.453592
 
 convertBtn.addEventListener("click", function() {
     let baseValue = numberInput.value
 
-    lengthEl.textContent = `${baseValue} meter = ${round(baseValue * meterToFeet)} feet`
-    volumeEl.textContent = `${baseValue} liter = ${round(baseValue * literToGallon)} gallons`
-    massEl.textContent = `${baseValue} kilos = ${round(baseValue * kiloToPound)} lbs`
+    lengthEl.textContent = `${baseValue} Meters = ${round(baseValue * meterToFeet)} Feet
+        | ${baseValue} Feet = ${round(baseValue * feetToMeter)}`
+    volumeEl.textContent = `${baseValue} Liters = ${round(baseValue * literToGallon)} Gallons 
+        | ${baseValue} Gallons = ${round(baseValue * gallonToLiter)}`
+    massEl.textContent = `${baseValue} Kilos = ${round(baseValue * kiloToPound)} Pounds 
+        | ${baseValue} Pounds = ${round(baseValue * poundToKilo)}`
 })
 
 function round(num) {
